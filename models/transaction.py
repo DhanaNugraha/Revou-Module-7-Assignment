b6a002a1a3e0f0ac50bfe95f0eb7804b5fa8e818
DhanaNugraha <dhananugraha1511@gmail.com>
2025-03-12 13:15:59 +0700

    feat: initialization of project and the db

---
+++ models/transaction.py
@@ -0,0 +1,38 @@
+transactions_db = {
+    "transactions": {
+        "a1" : {
+            "a1t1" : {
+                "transactionId": "a1t1",
+                "accountId": "a1",
+                "type": "debit",
+                "amount": 100.00,
+                "currency": "USD",
+                "description": "Grocery store purchase",
+                "transactionDate": "01-02-2023",
+                "status": "completed",
+            },
+            "a1t2" : {
+                "transactionId": "a1t2",
+                "accountId": "a1",
+                "type": "credit",
+                "amount": 500.00,
+                "currency": "USD",
+                "description": "Salary deposit",
+                "transactionDate": "05-10-2023",
+                "status": "completed",
+            },
+        },
+        "a2" : {
+            "a2t1" : {
+                "transactionId": "a2t1",
+                "accountId": "a2",
+                "type": "debit",
+                "amount": 200.00,
+                "currency": "USD",
+                "description": "Utility bill payment",
+                "transactionDate": "02-11-2023",
+                "status": "completed",
+            },
+        }
+    }
+}
---
+++ models/user.py
@@ -0,0 +1,40 @@
+users_db = {
+    "users": {
+        "u1": {
+            "userId": "u1",
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "john.doe@example.com",
+            "phoneNumber": "+1234567890",
+            "address": {
+                "street": "123 Main St",
+                "city": "New York",
+                "state": "NY",
+                "postalCode": "10001",
+                "country": "USA",
+            },
+            "dateOfBirth": "1990-01-01",
+            "createdAt": "01-01-2023",
+            "accounts": {"1": "A1", "2": "A2"},
+        },
+        "u2": {
+            "userId": "u2",
+            "firstName": "Jane",
+            "lastName": "Smith",
+            "email": "jane.smith@example.com",
+            "phoneNumber": "+0987654321",
+            "address": {
+                "street": "456 Elm St",
+                "city": "Los Angeles",
+                "state": "CA",
+                "postalCode": "90001",
+                "country": "USA",
+            },
+            "dateOfBirth": "1985-05-15",
+            "createdAt": "15-02-2023",
+            "accounts": {
+                "1": "a3",
+            },
+        },
+    }
+}
