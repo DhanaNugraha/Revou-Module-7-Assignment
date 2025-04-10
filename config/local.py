72ee3440fb570114bd61e4889004391d1e3e45a0
DhanaNugraha <dhananugraha1511@gmail.com>
2025-04-10 19:35:53 +0700

    feat: removed local config

--- config/local.py
+++
@@ -1,7 +0,0 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres.ntkmyddmxvzhnsbvcwlk:Erk62CsBHInWx52s@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
-
-JWT_SECRET_KEY = "revou-milestone-3"
-
-JWT_ACCESS_TOKEN_EXPIRES = 180
-
-
